af899a59cfc9dac491fe6f16b221ea2c5fd6fd27
Rekefisk47 <millajooy@gmail.com>
2025-02-09 18:28:22 +0100

    Added ABtester middleware (demo).

--- server.mjs
+++ server.mjs
@@ -11,8 +11,10 @@ import { shuffleDeck } from "./temp/deck.mjs";
 import { drawCard } from "./temp/deck.mjs";
 import crypto from 'crypto';
 //Uke 5
+import path from 'path';
 import log from './modules/log.mjs';
 import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
+import { storeSession, cookies, abTesting } from './modules/session.mjs';
 //----------------------------------------
 const ENABLE_LOGGING = false;
 
@@ -20,22 +22,27 @@ const server = express();
 const port = (process.env.PORT || 8000);
 
 const logger = log(LOGG_LEVELS.VERBOSE);
+
+//you can turn AB testing on and off
+const abTestingOn = abTesting(true); 
 //----------------------------------------
 server.set('port', port);
 server.use(logger);
 server.use(express.static('public'));
 server.use(express.json());
-//----------------------------------------
 
+server.use(storeSession);
+//----------------------------------------
+ 
 server.get("/", (req, res, next) => {
     eventLogger("Noen spurte etter root");
-    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
+    res.status(HTTP_CODES.SUCCESS.OK).send(`Hello World`).end();
 });
 
 //---------------------------------------Uke-3---------------------------------------//
 
 server.get("/tmp/poem", (req, res, next) => {
-    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem + "<br><br>" + translate[req.language].poem).end();
 });
 
 server.get("/tmp/quote", (req, res, next) => {
@@ -97,6 +104,20 @@ server.get("/temp/deck/:deck_id/card", (req, res, next) => {
 });
 
 //---------------------------------------Uke-4-END-----------------------------------//
+
+//---------------------------------------Uke-5---------------------------------------//
+
+//Endpoint for a visual demonstration of the AB test:
+server.get("/show_ab_testing", (req, res, next) => {
+    res.status(HTTP_CODES.SUCCESS.OK).sendFile(path.resolve('public', 'abTest.html'));
+});
+
+server.get("/get_cookie", (req, res, next) => {
+    let decodedCookie = decodeURIComponent(cookies.session); 
+    res.send(decodedCookie);
+});
+
+//---------------------------------------Uke-5-END-----------------------------------//
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
