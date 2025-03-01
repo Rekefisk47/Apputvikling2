fc7ea31f16bd2275a0a028abca06ab35fb959664
Rekefisk47 <millajooy@gmail.com>
2025-03-01 19:37:00 +0100

    Removed the rest of Poem/Quote/Sum.

--- tmp/poem.mjs
+++
@@ -1,4 +0,0 @@
-
-const poem = "Twinkle twinkle little star, I want to hit you with my car";
-
-export default poem;
--- tmp/sum.mjs
+++
@@ -1,8 +0,0 @@
-
-
-export function calculateSum(a,b){
-    const sum = parseInt(a) + parseInt(b);
-    return sum;
-}
-
-
