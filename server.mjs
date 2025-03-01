db2e930e1ccad97d0f9f33f5544ef3eb39d1c455
Rekefisk47 <millajooy@gmail.com>
2025-03-01 21:25:27 +0100

    BENEATH: Removed a lot of unused files from fork. Cleaned up project to make it ready for the final product.

--- server.mjs
+++ server.mjs
@@ -1,30 +1,32 @@
+
 import express from 'express'
 import HTTP_CODES from './utils/httpCodes.mjs';
-//Uke 5
 import log from './modules/log.mjs';
 import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
-//Uke 6
 import userRouter from './routes/userAPI.mjs';
-//Uke 8
 import hashmapRouter from './routes/hashmapAPI.mjs';
-//uke 9
-import path from 'path';
-//----------------------------------------
+
+//----------------------------------------//
+
 const ENABLE_LOGGING = false;
 
 const server = express();
 const port = (process.env.PORT || 8000);
 
 const logger = log(LOGG_LEVELS.VERBOSE);
-//----------------------------------------
+
+//----------------------------------------//
+
+//Middleware setup
 server.set('port', port);
 server.use(logger);
 server.use(express.static('public'));
 server.use(express.json());
-//Uke 6
+
+//API Routers
 server.use("/user", userRouter)
-//Uke 8
 server.use("/hashmap", hashmapRouter);
+
 //----------------------------------------//
  
 server.get("/", (req, res, next) => {
@@ -37,6 +39,5 @@ server.get("/", (req, res, next) => {
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
-
 
 export default server;
