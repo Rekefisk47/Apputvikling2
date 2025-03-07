9a4733246513c35a1c3771085be4ace71e2212f2
Rekefisk47 <millajooy@gmail.com>
2025-03-07 04:05:14 +0100

    Added a few more Http-codes.

--- utils/httpCodes.mjs
+++ utils/httpCodes.mjs
@@ -9,11 +9,14 @@ const HTTP_CODES = {
         BAD_REQUEST: 400,
         UNAUTHORIZED: 401,
         NOT_FOUND: 404,
+        CONFLICT: 409,
+        GONE: 410,
         IM_A_TEAPOT: 418
     },
     SERVER_ERROR: {
         INTERNAL_SERVER_ERROR: 500,
         NOT_IMPLEMENTED: 501,
+        BAD_GATEWAY: 501,
         SERVICE_UNAVAILABLE: 503
     },
 }
