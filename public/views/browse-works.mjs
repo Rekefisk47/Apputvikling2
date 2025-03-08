d067403b5d48cb6dfd8508ef5f3de4ab691def3d
Rekefisk47 <millajooy@gmail.com>
2025-03-08 04:00:06 +0100

    Made browse-works view. It lists out all works and you can click on them to read.

--- public/views/browse-works.mjs
+++ public/views/browse-works.mjs
@@ -1,7 +1,29 @@
-import { placeTemplate } from "../modules/load-views.mjs";
+import { placeTemplate } from "../modules/load-templates.mjs";
+import { getAllWorks } from "../modules/fetch-data.mjs";
 
-export function init() {
-    console.log("Browse works script loaded");
+export async function init(pageData = null) {
 
-    
+    let worksContainer = document.getElementById("works-container");
+    let works = await getAllWorks();
+    console.log(works);
+    Object.entries(works).forEach(([key, value]) => {
+        /*console.log('----------------------');
+        console.log(key, value);*/
+        
+        let div = document.createElement("div");
+        div.id = key;
+        div.innerHTML = `Work-id: ${key} <br> Title: <a href="#" id="link">${value.title}</a> <br> Author: ${value.author} <br>`;
+        if(value.summary){
+            div.innerHTML += `Summary: ${value.summary} <br>`;
+        }
+        div.innerHTML += `<hr>`;
+
+        worksContainer.appendChild(div);
+
+        const link = div.querySelector("#link");
+        link.addEventListener("click", (evt) => {
+            evt.preventDefault();
+            placeTemplate("show-work-template.html", "show-work.mjs", value);
+        });
+    });
 }
