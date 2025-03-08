18638d339c3419b0c043b6ce33abbf2ef952e3ec
Rekefisk47 <millajooy@gmail.com>
2025-03-08 04:02:17 +0100

    Made views for logging in and creating users.

--- public/views/create-user.mjs
+++ public/views/create-user.mjs
@@ -1,7 +1,30 @@
-import { placeTemplate } from "../modules/load-views.mjs";
+import { placeTemplate } from "../modules/load-templates.mjs";
+import { formDataToObject } from "../modules/formData-to-object.mjs";
+import { addUser } from "../modules/fetch-data.mjs";
+import { messagehandler } from "../modules/message-handler.mjs";
 
-export function init() {
+export function init(pageData = null) {
     console.log("Create user script loaded");
 
+    document.getElementById("login-user-link").addEventListener("click", () => {
+        placeTemplate("login-user-template.html", "login-user.mjs");
+    });
 
+    const createUserForm = document.getElementById("create-user-form");
+    
+    createUserForm.addEventListener("submit", async evt => {
+        evt.preventDefault();
+
+        const formData = new FormData(createUserForm);
+        const formDataObj = formDataToObject(formData);
+        
+        //fetch response 
+        let response =  await addUser(formDataObj);
+        if(response.status){
+            messagehandler(response.message);
+            placeTemplate("login-user-template.html", "login-user.mjs");
+        }else{
+            messagehandler(response.message);
+        }
+    });
 }
--- public/views/login-user.mjs
+++ public/views/login-user.mjs
@@ -1,9 +1,28 @@
-import { placeTemplate } from "../modules/load-views.mjs";
+import { placeTemplate } from "../modules/load-templates.mjs";
+import { formDataToObject } from "../modules/formData-to-object.mjs";
+import { loginUser } from "../modules/fetch-data.mjs";
+import { messagehandler } from "../modules/message-handler.mjs";
 
-export function init() {
-    console.log("Login user script loaded");
+export function init(pageData = null) {
 
     document.getElementById("create-user-link").addEventListener("click", () => {
         placeTemplate("create-user-template.html", "create-user.mjs");
+    });
+
+    const loginUserForm = document.getElementById("login-user-form");
+    
+    loginUserForm.addEventListener("submit", async evt => {
+        evt.preventDefault();
+        const formData = new FormData(loginUserForm);
+        const formDataObj = formDataToObject(formData);
+
+        //fetch response 
+        let response =  await loginUser(formDataObj);
+        if(response.status){
+            messagehandler(response.message);
+            placeTemplate("home-template.html", "home.mjs");
+        }else{
+            messagehandler(response.message);
+        }
     });
 }
